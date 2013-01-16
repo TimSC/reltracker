81f126ecd54b983398bedd2e57afd22207909e7b
TimSC <tim2009@sheerman-chase.org.uk>
2013-01-16 21:30:40 +0000

    Load training data

--- reltracker.py
+++ reltracker.py
@@ -1,6 +1,6 @@
 
 from PIL import Image
-import time, math, pickle
+import time, math, pickle, sys
 import numpy as np
 import sklearn.ensemble as ensemble
 
@@ -24,11 +24,46 @@ def BilinearSample(imgPix, x, y):
 def ITUR6012(col): #ITU-R 601-2
 	return 0.299*col[0] + 0.587*col[1] + 0.114*col[2]
 
+def ReadPosData(fina):
+	data = open(fina).readlines()
+	numFrames = int(data[0])
+	pos = 1
+	out = {}
+
+	for frameNum in range(numFrames):
+		numPts = int(data[pos])
+		frameTime = int(data[pos+1])
+		pts = []
+		for ptNum in range(numPts):
+			pt = map(float,data[pos+2+ptNum].strip().split(" "))
+			pts.append(pt)
+		out[frameTime] = pts
+
+		pos += 2 + numPts
+	return out
+
+#*******************************************************************************
+
 class RelTracker:
 	def __init__(self):
 		pass
 
+	def Add(self, im, pos):
+		pass
+
+if __name__ == "__main__":
+	posData = ReadPosData(sys.argv[1])
+
+	reltracker = RelTracker()
+	for ti in posData:
+		imgFina = sys.argv[2]+"/{0:05d}.png".format(ti)
+		print ti, imgFina
+		im = Image.open(imgFina)
+
+		reltracker.Add(im, posData[ti])
 
 
+
+	
 
 
