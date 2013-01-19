c72bc7a190c5efbad802433cf184152866bc9f06
TimSC <tim2009@sheerman-chase.org.uk>
2013-01-19 21:08:55 +0000

    Updated bilinear inner code

--- relutil.pyx
+++ relutil.pyx
@@ -4,31 +4,40 @@
 # cython: wraparound=False
 
 import cmath, math
+cimport numpy as np
+import numpy as np
 
 cdef BilinearSample(imgPix, float x, float y):
-	cdef float xfrac, xi, yfrac, yi
 
-	xfrac, xi = math.modf(x)
-	yfrac, yi = math.modf(y)
+	cdef np.ndarray[np.uint8_t, ndim=2] p = np.empty((4, imgPix.shape[2]), dtype=np.uint8)
+	cdef np.ndarray out = np.empty((imgPix.shape[2]))
+	cdef int c
+	cdef int xi = int(x)
+	cdef double xfrac = x - xi
+	cdef int yi = int(y)
+	cdef double yfrac = y - yi
+	cdef double c1, c2
+
+	#Check bounds
+	if xi < 0 or xi + 1 >= imgPix.shape[1]:
+		raise IndexError("Pixel location outside image")
+	if yi < 0 or yi + 1 >= imgPix.shape[0]:
+		raise IndexError("Pixel location outside image")
 
 	#Get surrounding pixels
-	p00 = imgPix[yi,xi,:]
-	p10 = imgPix[yi,xi+1,:]
-	p01 = imgPix[yi+1,xi,:]
-	p11 = imgPix[yi+1,xi+1,:]
-
-	#If a single number has been returned, convert to list
-	if not hasattr(p00, '__iter__'): p00 = [p00]
-	if not hasattr(p10, '__iter__'): p10 = [p10]
-	if not hasattr(p01, '__iter__'): p01 = [p01]
-	if not hasattr(p11, '__iter__'): p11 = [p11]
+	for c in range(imgPix.shape[2]):
+		p[0,c] = imgPix[yi, xi, c]
+		p[1,c] = imgPix[yi, xi+1, c]
+		p[2,c] = imgPix[yi+1, xi, c]
+		p[3,c] = imgPix[yi+1, xi+1, c]
 
 	#Interpolate colour
-	c1 = [p00c * (1.-xfrac) + p10c * xfrac for p00c, p10c in zip(p00, p10)]
-	c2 = [p01c * (1.-xfrac) + p11c * xfrac for p01c, p11c in zip(p01, p11)]
-	col = [c1c * (1.-yfrac) + c2c * yfrac for c1c, c2c in zip(c1, c2)]
+	for i in range(imgPix.shape[2]):
+		c1 = p[0,c] * (1.-xfrac) + p[1,c] * xfrac
+		c2 = p[2,c] * (1.-xfrac) + p[3,c] * xfrac
+		out[c] = c1 * (1.-yfrac) + c2 * yfrac
 
-	return col
+	return out
 
 def GetPixIntensityAtLoc(iml, supportOffsets, float locx, float locy, float rotation = 0.):
 	cdef float x, y, rx, ry
