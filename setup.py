724e48c91735e0ce407f3348ed9736742bdca079
Tim Sheerman-Chase <t.sheerman-chase@surrey.ac.uk>
2013-01-21 19:15:08 +0000

    Adding numpy path

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy as np
 
-ext_modules = [Extension("relutil", ["relutil.pyx"])]
+extmods = [Extension("relutil", ["relutil.pyx"], include_dirs=[np.get_include()])]
 
 setup(
   name = 'reltracker',
   cmdclass = {'build_ext': build_ext},
-  ext_modules = ext_modules
+  ext_modules = extmods
 )
 
 #python setup.py build_ext --inplace
